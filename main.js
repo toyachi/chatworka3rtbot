b7c1739cf5d84de9b386f04c05456b1127ea39d3
外山雄一 <yuichi@toyamayuuichinoMacBook-Pro.local>
2017-12-10 22:31:22 +0900

    issue#6 おうむ返し機能

--- main.js
+++ main.js
@@ -8,9 +8,12 @@ setInterval( function() {
   cwapi.getmsg(function(cwres){
     for (var i=0; i<cwres.length; i++){
       var rpmsg = '[rp aid=' + cwres[i].account.account_id + ' to=' + _conf._cw._rid + '-' + cwres[i].message_id + ']' + cwres[i].account.name + '\r\n';
-      talkapi.sendmsg(cwres[i].body,function(botmsg) {
-        if(Math.floor( Math.random() * 100) > 80){
+      talkapi.sendmsg(cwres[i].body,function(botmsg,usermsg) {
+        var randnum = Math.floor( Math.random() * 100);
+        if(randnum > 80){
           botmsg = _conf._provocativemsg();
+        }else if(randnum > 70){
+          botmsg = '「' + usermsg + '」じゃねーよｗｗｗ'
         }
         cwapi.sendmsg(rpmsg + botmsg);
         sendtime = new Date();
--- talkapi.js
+++ talkapi.js
@@ -7,23 +7,23 @@ var options = {
 };
 var talkapi = new Object();
 
-talkapi.sendmsg = function(msg,callback){
+talkapi.sendmsg = function(usermsg,callback){
   var form = {
           apikey: _conf._talk._apikey,
-          query: msg
+          query: usermsg
       };
   options.form = form;
 
   request.post(options, function (error, response, body) {
     var botmsg = '';
     if (!error && body.status == 0) {
-      botmsg = _conf._eow(msg, body.results[0].reply);
+      botmsg = _conf._eow(usermsg, body.results[0].reply);
     }else if (!error && body.status == 2000) {
       botmsg = '・・・|-)';
     }else{
       botmsg = 'ERROR：HTTP status ' + response.statusCode + ' ' + body.message;
     }
-    callback(botmsg);
+    callback(botmsg,usermsg);
   });
 }
 
